130813ca329f06fa9b99a42bedac51648d8eb9a0
Adrian Thomas <adrianth@usc.edu>
2026-01-29 17:12:20 -0800

    updated files

--- tcp_client.py
+++ tcp_client.py
@@ -16,15 +16,15 @@ def main():
     HOST = "172.20.10.5"  # The server's hostname or IP address
     PORT = 5555  # The port used by the server
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        while True:
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
             message = input()
             s.sendall(message.encode('utf-8'))
             data = s.recv(1024)
             print(data)
 
-    print(f"Received {data!r}")
+            print(f"Received {data!r}")
 
     
 
